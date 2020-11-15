7c54854589a67b6b176fe7f4e648a4247963088b
kym1924 <kym1924@naver.com>
2020-11-15 13:41:43 +0900

    [UPDATE] BindingAdapter for MainActivity

--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/main/MainActivity.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/main/MainActivity.kt
@@ -2,20 +2,25 @@ package com.example.retrofitpractice.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.retrofitpractice.R
+import com.example.retrofitpractice.databinding.ActivityMainBinding
 import com.example.retrofitpractice.util.addMainPagerListener
 import com.example.retrofitpractice.util.addTabLayoutListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        vp_main.addMainPagerListener(tab_main)
-        vp_main.adapter = MainPagerAdapter(supportFragmentManager)
-        tab_main.addTabLayoutListener(vp_main)
-        tab_main.addTab(tab_main.newTab().setText("Users"))
-        tab_main.addTab(tab_main.newTab().setText("Repo"))
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply{
+            fragmentManager = supportFragmentManager
+            viewPager = binding.vpMain
+            tabLayout = binding.tabMain
+            tabItems = "USER REPO"
+            lifecycleOwner = this@MainActivity
+        }
     }
 }
