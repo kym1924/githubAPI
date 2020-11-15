c36551d4e6774648380e0e11c0bd9bd58828da7b
kym1924 <kym1924@naver.com>
2020-11-16 00:01:12 +0900

    [UPDATE] UserViewModel.requestUsers

--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/user/UserViewModel.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/user/UserViewModel.kt
@@ -1,13 +1,27 @@
 package com.example.retrofitpractice.ui.user
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.retrofitpractice.data.api.SearchRepository
+import com.example.retrofitpractice.data.model.UsersData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UserViewModel : ViewModel() {
 
     private lateinit var repository : SearchRepository
 
+    private val _allData = MutableLiveData<UsersData>()
+    val allData : LiveData<UsersData>
+        get() = _allData
+
     fun init(searchRepository : SearchRepository) {
         this.repository = searchRepository
+    }
+
+    fun requestUsers(q : String) = viewModelScope.launch(Dispatchers.IO) {
+        _allData.postValue(repository.requestUsers(q))
     }
 }
