5f795cfcbb076138625c6ed92aa37103791909e3
kym1924 <kym1924@naver.com>
2020-11-15 14:45:17 +0900

    [UPDATE] SearchAdapter

---
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/search/SearchAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.retrofitpractice.ui.search
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofitpractice.R
+import com.example.retrofitpractice.data.Search
+import com.example.retrofitpractice.ui.main.MainActivity
+import com.example.retrofitpractice.util.startActivity
+
+class SearchAdapter(private val context : Context, private val searchViewModel : SearchViewModel) : RecyclerView.Adapter<SearchAdapter.VHolder>(){
+    var search = emptyList<Search>()
+
+    override fun onCreateViewHolder(parent : ViewGroup, viewType : Int)
+            = VHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_search_history, parent, false))
+
+    override fun getItemCount() = search.size
+
+    override fun onBindViewHolder(holder: VHolder, position: Int) {
+        holder.bind(search[position])
+    }
+
+    internal fun setData(search : List<Search>) {
+        this.search = search
+        notifyDataSetChanged()
+    }
+
+    inner class VHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val search : TextView = itemView.findViewById(R.id.tv_latest_search)
+
+        fun bind(data : Search) {
+            search.text = data.search
+            search.setOnClickListener { context.startActivity<MainActivity>() }
+
+            itemView.findViewById<ImageView>(R.id.img_delete).setOnClickListener { searchViewModel.delete(data) }
+        }
+    }
+}
