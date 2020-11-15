ed6d1705ba5fb9dedd00688696810fa30d507e85
kym1924 <kym1924@naver.com>
2020-11-15 14:03:16 +0900

    [UPDATE] MainActivity <-> SearchActivity

--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/main/MainActivity.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/main/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.retrofitpractice.R
 import com.example.retrofitpractice.databinding.ActivityMainBinding
+import com.example.retrofitpractice.ui.search.SearchActivity
 import com.example.retrofitpractice.util.addMainPagerListener
 import com.example.retrofitpractice.util.addTabLayoutListener
+import com.example.retrofitpractice.util.startActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +23,17 @@ class MainActivity : AppCompatActivity() {
             tabLayout = binding.tabMain
             tabItems = "USER REPO"
             lifecycleOwner = this@MainActivity
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.etSearch.clearFocus()
+        binding.etSearch.setOnFocusChangeListener{ _, hasFocus ->
+            if(hasFocus) {
+                startActivity<SearchActivity>()
+                finish()
+            }
         }
     }
 }
--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/search/SearchActivity.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/search/SearchActivity.kt
@@ -1,12 +1,31 @@
 package com.example.retrofitpractice.ui.search
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import com.example.retrofitpractice.R
+import com.example.retrofitpractice.ui.main.MainActivity
+import com.example.retrofitpractice.util.startActivity
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        et_search.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                startActivity<MainActivity>()
+                finish()
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
     }
 }
