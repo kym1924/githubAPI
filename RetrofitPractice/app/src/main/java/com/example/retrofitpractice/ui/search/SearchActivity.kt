82329a48a53aeac385ec0549b6a347f65d956dc2
kym1924 <kym1924@naver.com>
2020-11-15 14:45:26 +0900

    [UPDATE] SearchActivity for database

--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/search/SearchActivity.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/search/SearchActivity.kt
@@ -5,9 +5,11 @@ import android.view.inputmethod.EditorInfo
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.retrofitpractice.R
 import com.example.retrofitpractice.databinding.ActivitySearchBinding
 import com.example.retrofitpractice.ui.main.MainActivity
+import com.example.retrofitpractice.util.room.SearchDatabase
 import com.example.retrofitpractice.util.startActivity
 import kotlinx.android.synthetic.main.activity_search.*
 
@@ -20,12 +22,27 @@ class SearchActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
         binding.searchViewModel = searchViewModel
         binding.lifecycleOwner = this
+
+        val searchDao = SearchDatabase.getDatabase(this).searchDao()
+        searchViewModel.init(searchDao)
     }
 
+    override fun onStart() {
+        super.onStart()
+        val adapter = SearchAdapter(this, searchViewModel)
+
+        searchViewModel.allData.observe(this, Observer { allData ->
+            allData?.let {
+                binding.rvSearch.adapter = adapter
+                adapter.setData(allData)
+            }
+        })
+    }
     override fun onResume() {
         super.onResume()
         et_search.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                searchViewModel.insert()
                 startActivity<MainActivity>()
                 finish()
                 return@setOnEditorActionListener true
