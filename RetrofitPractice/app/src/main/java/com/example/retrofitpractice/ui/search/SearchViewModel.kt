c165caf3dc2c18fb9cafa76a4b8c9223527361e1
kym1924 <kym1924@naver.com>
2020-11-15 14:44:22 +0900

    [UPDATE] SearchViewModel for database

--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/search/SearchViewModel.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/search/SearchViewModel.kt
@@ -1,11 +1,33 @@
 package com.example.retrofitpractice.ui.search
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.retrofitpractice.data.Search
+import com.example.retrofitpractice.util.room.SearchDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SearchViewModel : ViewModel() {
 
     val search = MutableLiveData<String>("")
+    private lateinit var searchDao : SearchDao
+    lateinit var allData : LiveData<List<Search>>
 
     fun resetSearch() { search.value="" }
+
+    fun init(dao : SearchDao) {
+        searchDao = dao
+        allData = searchDao.getAll()
+    }
+
+    fun insert() = viewModelScope.launch(Dispatchers.IO) {
+        val search = Search(idx = 0, search = search.value!!)
+        searchDao.insert(search)
+    }
+
+    fun delete(search : Search) = viewModelScope.launch(Dispatchers.IO) {
+        searchDao.delete(search)
+    }
 }
