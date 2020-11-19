1bb23593d50271dc88fd727303af378bca33df82
kym1924 <kym1924@naver.com>
2020-11-19 19:16:44 +0900

    [REFACTOR] Setting ResultAdapter

--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/repo/ReposBinding.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/repo/ReposBinding.kt
@@ -4,18 +4,19 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.retrofitpractice.data.model.ReposItems
 import com.example.retrofitpractice.databinding.ItemSearchRepoBinding
+import com.example.retrofitpractice.ui.adapter.ResultAdapter
 
 object ReposBinding {
-    @BindingAdapter("setRepoAdapter")
-    @JvmStatic
-    fun setRepoAdapter(recyclerView : RecyclerView, layout : Int) {
-        val adapter = ReposAdapter<ItemSearchRepoBinding>(layout)
-        recyclerView.adapter = adapter
-    }
-
     @BindingAdapter("setRepoItem")
     @JvmStatic
-    fun setRepoItem(recyclerView : RecyclerView, reposList : List<ReposItems>?) {
-        if (recyclerView.adapter != null) with(recyclerView.adapter as ReposAdapter<*>) { reposList?.let{ setData(it) } }
+    fun setRepoItem(recyclerView: RecyclerView, reposList: List<ReposItems>?) {
+        if (recyclerView.adapter != null) with(recyclerView.adapter as ResultAdapter<*, ReposItems>) { reposList?.let { setData(it)} }
+    }
+
+    @BindingAdapter("setRepoAdapter")
+    @JvmStatic
+    fun setRepoAdapter(recyclerView: RecyclerView, layout: Int) {
+        val adapter = ResultAdapter<ItemSearchRepoBinding, ReposItems>(layout)
+        recyclerView.adapter = adapter
     }
 }
--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/user/UsersBinding.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/user/UsersBinding.kt
@@ -4,18 +4,19 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.retrofitpractice.data.model.UsersItems
 import com.example.retrofitpractice.databinding.ItemSearchUserBinding
+import com.example.retrofitpractice.ui.adapter.ResultAdapter
 
 object UsersBinding {
-    @BindingAdapter("setUserAdapter")
-    @JvmStatic
-    fun setUserAdapter(recyclerView : RecyclerView, layout : Int) {
-        val adapter = UsersAdapter<ItemSearchUserBinding>(layout)
-        recyclerView.adapter = adapter
-    }
-
     @BindingAdapter("setUserItem")
     @JvmStatic
     fun setUserItem(recyclerView : RecyclerView, usersList : List<UsersItems>?) {
-        if (recyclerView.adapter != null) with(recyclerView.adapter as UsersAdapter<*>) { usersList?.let{ setData(it) } }
+        if (recyclerView.adapter != null) with(recyclerView.adapter as ResultAdapter<*, UsersItems>) { usersList?.let{ setData(it) } }
+    }
+
+    @BindingAdapter("setUserAdapter")
+    @JvmStatic
+    fun setUserAdapter(recyclerView: RecyclerView, layout: Int) {
+        val adapter = ResultAdapter<ItemSearchUserBinding, UsersItems>(layout)
+        recyclerView.adapter = adapter
     }
 }
