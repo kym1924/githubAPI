d623b471c4f9f2d8995eaee1c77972dc7299434c
kym1924 <kym1924@naver.com>
2020-11-20 17:34:16 +0900

    [REFACTOR] Rename users -> list

--- RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/adapter/ResultAdapter.kt
+++ RetrofitPractice/app/src/main/java/com/example/retrofitpractice/ui/adapter/ResultAdapter.kt
@@ -9,19 +9,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.retrofitpractice.BR
 
 class ResultAdapter<B : ViewDataBinding, I>(private val layout : Int) : RecyclerView.Adapter<ResultAdapter<B, I>.VHolder<B, I>>(){
-    private var users = emptyList<I>()
+    private var list = emptyList<I>()
 
     override fun onCreateViewHolder(parent : ViewGroup, viewType : Int)
             = VHolder<B, I>(LayoutInflater.from(parent.context).inflate(layout, parent, false))
 
-    override fun getItemCount() = users.size
+    override fun getItemCount() = list.size
 
     override fun onBindViewHolder(holder: VHolder<B, I>, position: Int) {
-        holder.bind(users[position])
+        holder.bind(list[position])
     }
 
-    internal fun setData(users : List<I>) {
-        this.users = users
+    internal fun setData(list : List<I>) {
+        this.list = list
         notifyDataSetChanged()
     }
 
