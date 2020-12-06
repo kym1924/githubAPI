1991d63494ef0b0b0cd687a1b6188eda40416e94
kym1924 <kym1924@naver.com>
2020-12-06 22:46:04 +0900

    [REFACTOR] SearchActivity

--- RetrofitPractice/app/src/main/java/com/retrofit/github/ui/search/SearchActivity.kt
+++ RetrofitPractice/app/src/main/java/com/retrofit/github/ui/search/SearchActivity.kt
@@ -6,8 +6,13 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.retrofit.github.R
 import com.retrofit.github.databinding.ActivitySearchBinding
+import com.retrofit.github.databinding.ItemSearchHistoryBinding
+import com.retrofit.github.ui.adapter.SearchAdapter
+import com.retrofit.github.ui.adapter.SearchPagerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,26 +25,35 @@ class SearchActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
         binding.apply{
             viewModel = searchViewModel
-            fragmentManager = supportFragmentManager
-            viewPager = binding.vpMain
-            tabLayout = binding.tabMain
             lifecycleOwner = this@SearchActivity
         }
 
+        init()
+        setObserver()
+        setFocusChangeListener(binding)
+        setEditorActionListener(binding)
+        addTabLayoutListener(binding)
+        addMainPagerListener(binding)
+        setAdapter(binding)
+    }
+
+    private fun init() {
         searchViewModel.init()
     }
 
-    override fun onStart() {
-        super.onStart()
+    private fun setObserver() {
         searchViewModel.search.observe(this, Observer { search ->
-            search?.let { if(binding.tabMain.selectedTabPosition==0) searchViewModel.requestUsers() else searchViewModel.requestRepos() }
+            search?.let { if (binding.tabMain.selectedTabPosition == 0) searchViewModel.requestUsers() else searchViewModel.requestRepos() }
         })
     }
 
-    override fun onResume() {
-        super.onResume()
-        binding.etSearch.setOnFocusChangeListener{ _, _ -> searchViewModel.setVisibility() }
+    private fun setFocusChangeListener(binding : ActivitySearchBinding) {
+        binding.etSearch.setOnFocusChangeListener { _, _ ->
+            searchViewModel.setVisibility()
+        }
+    }
 
+    private fun setEditorActionListener(binding : ActivitySearchBinding) {
         binding.etSearch.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 if(binding.etSearch.text.toString().isNotEmpty()) {
@@ -51,5 +65,39 @@ class SearchActivity : AppCompatActivity() {
             }
             return@setOnEditorActionListener false
         }
+    }
+
+    private fun addTabLayoutListener(binding : ActivitySearchBinding){
+        binding.tabMain.addTab(binding.tabMain.newTab().setText("USER"))
+        binding.tabMain.addTab(binding.tabMain.newTab().setText("REPO"))
+
+        binding.tabMain.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabSelected(tab: TabLayout.Tab?) { binding.vpMain.currentItem = tab!!.position }
+        })
+    }
+
+    private fun addMainPagerListener(binding : ActivitySearchBinding){
+        binding.vpMain.adapter = SearchPagerAdapter(supportFragmentManager)
+
+        binding.vpMain.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {}
+
+            override fun onPageSelected(position: Int) { binding.tabMain.getTabAt(position)?.select() }
+        })
+    }
+
+    private fun setAdapter(binding : ActivitySearchBinding) {
+        val adapter = SearchAdapter<ItemSearchHistoryBinding>(searchViewModel)
+        binding.rvSearchHistory.adapter = adapter
     }
 }
--- RetrofitPractice/app/src/main/java/com/retrofit/github/ui/search/SearchBinding.kt
+++ RetrofitPractice/app/src/main/java/com/retrofit/github/ui/search/SearchBinding.kt
@@ -2,54 +2,15 @@ package com.retrofit.github.ui.search
 
 import android.view.View
 import androidx.databinding.BindingAdapter
-import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
 import com.retrofit.github.data.model.Search
-import com.retrofit.github.databinding.ItemSearchHistoryBinding
 import com.retrofit.github.ui.adapter.SearchAdapter
-import com.retrofit.github.ui.adapter.SearchPagerAdapter
-import com.retrofit.github.util.addMainPagerListener
-import com.retrofit.github.util.addTabLayoutListener
 
 object SearchBinding {
     @BindingAdapter("visibleResetButton")
     @JvmStatic
     fun visibleResetButton(view : View, search : String) {
         view.visibility = if (search.isEmpty()) View.INVISIBLE else View.VISIBLE
-    }
-
-    @BindingAdapter("mainAdapter")
-    @JvmStatic
-    fun mainAdapter(viewPager: ViewPager, fragmentManager: FragmentManager) {
-        viewPager.adapter = SearchPagerAdapter(fragmentManager)
-    }
-
-    @BindingAdapter("setTabListener")
-    @JvmStatic
-    fun setTabListener(tabLayout : TabLayout, viewPager : ViewPager) {
-        tabLayout.addTabLayoutListener(viewPager)
-    }
-
-    @BindingAdapter("setUpWithViewPager")
-    @JvmStatic
-    fun setUpWithViewPager(viewPager : ViewPager, tabLayout : TabLayout) {
-        viewPager.addMainPagerListener(tabLayout)
-    }
-
-    @BindingAdapter("setTabItems")
-    @JvmStatic
-    fun setTabItems(tabLayout : TabLayout, tabItems : String) {
-        tabLayout.addTab(tabLayout.newTab().setText(tabItems.split(" ")[0]))
-        tabLayout.addTab(tabLayout.newTab().setText(tabItems.split(" ")[1]))
-    }
-
-    @BindingAdapter("setAdapter")
-    @JvmStatic
-    fun setAdapter(recyclerView : RecyclerView, searchViewModel : SearchViewModel) {
-        val adapter = SearchAdapter<ItemSearchHistoryBinding>(searchViewModel)
-        recyclerView.adapter = adapter
     }
 
     @BindingAdapter("setListItem")
